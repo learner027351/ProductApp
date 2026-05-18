49abf2ccd5784d069f1a58502e2770309da6e125
Vineet Singh <Vineet.Singh@wuh-group.com>
2026-05-18 12:26:12 +0530

    App Connected to the Database using Migration.

---
+++ ProductApp/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+using ProductApp.Model;
+
+namespace ProductApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ ProductApp/Model/Product.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductApp.Model
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required]
+        public string ProductName { get; set; } = null!;
+
+        public double ProductPrice { get; set; }
+
+        public string? ProductDescription { get; set; }
+
+        public string? ProductCategory { get; set; }
+
+        public bool isExpire { get; set; }
+
+        public DateTime mfg_date { get; set; }
+
+    }
+}
