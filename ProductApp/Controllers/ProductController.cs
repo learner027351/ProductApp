3733f6855e690af1ffdf14782c675ec932182cf6
Vineet Singh <Vineet.Singh@wuh-group.com>
2026-05-18 14:09:02 +0530

    Added API Endpoints for Add,Update,Delete and Get

--- ProductApp/Controllers/ProductController.cs
+++ ProductApp/Controllers/ProductController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ProductApp.Data;
+using ProductApp.Model;
 
 namespace ProductApp.Controllers
 {
@@ -7,5 +9,97 @@ namespace ProductApp.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
+
+        private readonly AppDbContext _context;
+
+        public ProductController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("getAllProduct")]
+
+        public IActionResult getAllProducts()
+        {
+            try
+            {
+                var product = _context.Products.ToList();
+
+                if (product.Count == 0)
+                {
+                    return NotFound("no products are available");
+
+                }
+
+                return Ok(product);
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500, "Internal Server Error: " + ex.Message);
+            }
+        }
+
+        [HttpPost("AddProduct")]
+
+        public IActionResult AddProduct(Product Products)
+        {
+            _context.Products.Add(Products);
+            _context.SaveChanges();
+
+            return Ok("Product added successfully");
+        }
+
+        [HttpPut("UpdateProduct/{id}")]
+
+        public IActionResult UpdateProduct(Product Products,int id)
+        {
+
+            try
+            {
+                var isRecordAvailable = _context.Products.SingleOrDefault(s=>s.ProductId==id);
+                if (isRecordAvailable == null)
+                {
+                    return NotFound("Product Not Found");
+                }
+
+                isRecordAvailable.ProductName = Products.ProductName;
+                isRecordAvailable.ProductPrice = Products.ProductPrice;
+                isRecordAvailable.ProductDescription = Products.ProductDescription;
+                isRecordAvailable.isExpire = Products.isExpire;
+                isRecordAvailable.mfg_date = Products.mfg_date;
+                isRecordAvailable.ProductCategory = Products.ProductCategory;
+
+                _context.SaveChanges();
+
+                return Ok("Product added successfully");
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500, "Internal Server Error: "+ ex.Message);
+            }
+            
+        }
+
+        [HttpDelete("DeleteProduct/{id}")]
+
+        public IActionResult DeleteProdut(int id)
+        {
+            try
+            {
+                var record = _context.Products.SingleOrDefault(s => s.ProductId == id);
+                if(record == null)
+                {
+                    return NotFound("Product not found");
+                }
+                _context.Products.Remove(record);
+                return Ok("Product Delete");
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500, "internal Server Error: " + ex.Message);
+            }
+        }
+
+
     }
 }
